d2b1bc304cd3c3bf688606510634790adb02b912
Dev-Rishabh17 <noobtopro1717@gmail.com>
2026-03-05 17:56:45 +0530

    1 problem pushed

--- Twosum.cpp
+++ Twosum.cpp
@@ -1,21 +1,19 @@
 #include<iostream>
-#include <vector>//this declares the vector container to store the input array and output indices.
-#include <unordered_map> // Include the unordered_map header
-using namespace std;
+#include <vector>
+#include <unordered_map> 
 class Solution{
     public:
     vector<int> twoSum(vector<int>& nums, int target){
         int n = nums.size();
         unordered_map<int, int> mp;
-        for (int i = 0; i < n; i++) // Corrected loop syntax
+        for (int i = 0; i < n; i++) 
         {
             int complement = target - nums[i];
             if (mp.find(complement) != mp.end()){
                 return {mp[complement], i};
             }
-            mp[nums[i]] = i; // Corrected insertion logic
-        }
+            mp[nums[i]] = i; 
         return {};
-    }
+    }}
 
 };
